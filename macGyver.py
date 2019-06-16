39cfbe807963f31cb04e4981009c724cf82bbd5a
JEAN PENAN <jeanpenangoumou1@gmail.com>
2019-06-16 17:43:03 +0200

    Rename the names of the classes files.

---
+++ macGyver.py
@@ -0,0 +1,9 @@
+class MacGyver:
+    """This class define the position and the characteristic of the hero MacGyver"""
+
+
+    def __init__(self, Labyrinth):
+        """This method is the constructor of the class MacGyver"""
+        self.Labyrinth = Labyrinth
+
+       
