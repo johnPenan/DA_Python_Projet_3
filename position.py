dc22fe3c8ac97ac0d4bb8cacb99e46f032ff7bec
JEAN PENAN <jeanpenangoumou1@gmail.com>
2019-06-16 21:05:30 +0200

    Updated the Position methods  docstring.

--- position.py
+++ position.py
@@ -1,20 +1,24 @@
 class Position:
-    """This class the different position of Macgyver"""
+    """This class define the different position of Macgyver"""
     def __init__(self, x, y):
         self._x = x
         self._y = y
 
    
     def _get_up(self):
+        """This method define the up direction"""
         return Position(self._x-1, self._y)
 
     def _get_down(self):
+        """This method define the down direction"""
         return Position(self._x+1, self._y)
 
     def _get_left(self):
+        """This method define the left direction"""
         return Position(self._x, self._y-1)
 
     def _get_right(self):
+        """This method define the right direction"""
         return Position(self._x, self._y+1)
 
     def __repr__(self):
