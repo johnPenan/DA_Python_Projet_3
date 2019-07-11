40ed05393aa3432f7d17bd0e230bf2a5a0cc03f5
JEAN PENAN <jeanpenangoumou1@gmail.com>
2019-07-11 02:07:23 +0200

    Deleted pygame folder.

--- pygame/config/settings.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-HEIGHT = 480
-WIDTH = 640
-
-BACKGROUND = str(BASE_DIR / 'images' / 'background.jpg')
-MUSHROOM = str(BASE_DIR / 'images' / 'mushroom.jpg')
