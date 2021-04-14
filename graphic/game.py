7c3d9aee8daeae34740cd11fc150bceb47d3d89a
Jean Penan <jeanpenangoumou1@gmail.com>
2021-04-15 00:42:38 +0200

    Adding pygame code for windows implementation.

--- graphic/game.py
+++ graphic/game.py
@@ -1,6 +1,32 @@
+
 import pygame
 
 pygame.init()
 
-class Game():
+class Game:
     ''' Game to help MacGyver escape '''
+    def __init__(self):
+        self.window_resolution = (600, 600)
+        self.streets = pygame.image.load("../stage/streets.png")
+        self.walls = pygame.image.load("../stage/walls.png")
+        self.game_is_running = True
+        self.blank_color = (0, 0, 0)
+
+    def display(self):
+
+        pygame.display.set_caption("Macgyver Labyrinth Game")
+        window_surface = pygame.display.set_mode(self.window_resolution, pygame.RESIZABLE)
+
+        while self.game_is_running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.game_is_running = False
+
+            window_surface.fill(self.blank_color)
+            window_surface.blit(self.streets, [10, 10])
+            window_surface.blit(self.walls, [40, 40])
+            pygame.display.flip()
+
+
+game = Game()
+game.display()
