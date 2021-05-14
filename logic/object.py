830a2859066c60a22440691d63a2b58468a45c7e
Jean Penan <jeanpenangoumou1@gmail.com>
2021-05-14 18:18:03 +0200

    Adding object class and policy.

---
+++ logic/object.py
@@ -0,0 +1,6 @@
+class Object:
+    def __init__(self, load_image, position):
+        self.load_image = load_image
+        self.position = position
+        self.is_drop = False
+
---
+++ resources/constants.py
@@ -0,0 +1,9 @@
+import pygame
+window_resolution = (600, 600)
+
+streets = pygame.image.load("stage/streets.png")
+walls = pygame.image.load("stage/walls.png")
+departure = pygame.image.load("stage/departure.png")
+arrival = pygame.image.load("stage/arrival.png")
+hero = pygame.image.load("stage/macGyver.png")
+blank_color = (0, 0, 0)
